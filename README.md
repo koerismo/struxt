cc0d5aa294a4c8a115e23e8c45a7b6c2a0fe6f69
Koerismo <56563658+koerismo@users.noreply.github.com>
2022-11-13 18:45:09 -0500

    Begin work on V2

--- README.md
+++ README.md
@@ -1,45 +1,27 @@
-# struct.ts
-A javascript-typescript library for Python-style data structures.
+# ConstrucTS
+*The ultimate binary-packing-unpacking library!*
 
 
-## Descriptions
-A structure features two component types: Tokens and groups. The latter is where this library deviates from Python's `struct` module.
+## So what does it do?
+Glad you asked. This library makes it ridiculously easy to read binary data. How you may ask? By letting you mangle data in two steps: defining a binary template, and that's all. How does it work? Please stop asking questions!
 
-**Tokens** handle the data being packed and unpacked by the structure. A token consists of a data type and a length. These two attributes are defined in the structure by a number and a single character. The token objects are auto-created by the structure object on initialization.
+This library follows the same conventions as Python's `struct` module, but with the added abilities of including templates in other templates and setting data lengths on the fly.
 
-**Groups** allow you to group tokens together. A group consists of a series of tokens and a length. These attributes are defined in the structure by a number and a series of tokens surrounded by brackets. struct.ts features two types of groups: sticky groups and separating groups. The primary difference between the two is that sticky groups take a single slot to pack and unpack, that single slot being an array of the collapsed values. Sticky groups are defined in the structure with square brackets `[]`, whereas separating groups are defined with round brackets `()`.
+---
 
-## Replacements
-struct.ts also allows structs to have other structs defined within the structure. Replacements are evaluated during structure object construction, meaning that their contents are copied into a new group inside the struct. They can be defined by inserting a name in *fancy* brackets `{}`, and inserting an entry with a matching name into a dict, which must be supplied to the constructor as an additional argument.
+## Okay, just show me the code.
+Calm down. Jesus. Here's an example:
 
-# Examples
-```ts
-/* Simple pack and unpack. */
-const ExampleA  = new JStruct( "iiii" );          // Create a new struct.
-const packedA   = ExampleA.pack([ 1, 2, 3, 4 ]);  // Pack the specified data into a new Uint8Array
-const unpackedA = ExampleA.unpack( packedA );     // Unpack the Uint8Array back into the original format
-console.log( unpackedA );
-```
-```ts
-/* Pack a couple of strings with groups. */
-const ExampleB = new JStruct( "2(5s)" );               // Create a struct with two 5-letter strings
-const packedB  = ExampleB.pack([ "Hello", "World" ]);  // Pack our two strings into a Uint8Array
-console.log( packedB );
-```
-```ts
-/* Use sticky groups to smartly unpack data. */
-const ExampleC  = new JStruct( "4[2c]" );                // Create a struct of four two-char sticky groups
-const unpackedC = ExampleC.unpack([ 1,2,3,4,5,6,7,8 ]);  // Unpack some data.
-console.log( unpackedC );
-```
-```ts
-/* Use replacements to insert a struct within another struct */
-const Color    = new JStruct( "4c8s" );                        // Create a struct containing 3 chars and an 8-letter name
-const Palette  = new JStruct( "3{color}", {'color': Color} );  // Create another struct containing 3 copies of the first one
-const packedPalette = Palette.pack([                           // Pack some data
-	0,   0,   0,   0,   'None',
-	0,   0,   0,   255, 'Black',
-	255, 255, 255, 255, 'White',
-]);
-console.log( packedPalette );
+```js
+var example = Struct.new();
+example.add( D.STR, 'identifier', 4 );
+example.add( D.UINT8, 'body-length', 1 );
+example.add( D.CHAR, 'body', () => example.eval('body-length') );
+
+console.log( example.unpack( myData ) );
+/* {
+	'identifier': 'ABCD',
+	'body-length': 512,
+	'body': Uint8Array(512)
+} */
 ```
---
+++ src/datatype.ts
@@ -0,0 +1,215 @@
+/** This file defines the types used by Construct as well as their packing/unpacking methods. */
+
+/** Abstract integer type. */
+export type int = number;
+
+
+/** Shorthand single-letter identifiers for datatypes. */
+export const SHORT: {[key: string]: string } = {
+	INT8:	'i',
+	UINT8:	'I',
+	CHAR:	'c',
+	STR:	's',
+}
+
+/** Unique integer constant representations of datatypes. */
+export const DTYPE: {[key: string]: int }  = {
+	INT8: 		0,
+	INT16:		1,
+	INT32:		2,
+	INT64:		3,
+
+	UINT8:		4,
+	UINT16:		5,
+	UINT32:		6,
+	UINT64:		7,
+
+	FLOAT32:	8,
+	FLOAT64:	9,
+	FLOAT128:	10,
+
+	CHAR:		11,
+	STR:		12,
+
+	/** Padding: Packs to 0x00 bytes, skipped when unpacking. */
+	PADDING:	14,
+	/** Null: Skipped when packing, unpacks to a null array. */
+	NULL:		15,
+};
+
+/** Sizes of each data type in bytes-per-unit. */
+export const DSIZE: {[key: int]: int } = {
+	0:		1,		// INT8
+	1:		2,		// INT16
+	2:		4,		// INT32
+	3:		8,		// INT64
+
+	4:		1,		// UINT8
+	5:		2,		// UINT16
+	6:		4,		// UINT32
+	7:		8,		// UINT64
+
+	8:		4,		// FLOAT32
+	9:		8,		// FLOAT64
+	10:		16,		// FLOAT128
+
+	11:		1,		// CHAR
+	12:		1,		// STR
+
+	13:		1,		// PADDING
+	14:		1,		// NULL
+}
+
+
+/* Global encoders/decoders for text transforms. */
+const TEncoder = new TextEncoder();
+const TDecoder = new TextDecoder();
+
+
+/** This constant reports the current system's endianness. If true, the system is big-endian. This is used to perform various shortcuts in packing and unpacking. */
+export const system_endianness = (new Uint8Array(new Uint16Array([255]).buffer)[0] === 255);
+export const BIG_ENDIAN = false,
+             LITTLE_ENDIAN = true;
+
+
+/** Datatype pack function, used internally by Construct. */
+export function Pack( type:int, data:ArrayLike<number>|any|null, size:int, endianness:boolean ): Uint8Array {
+
+	// Switch/case doesn't work for this, because javascript
+	// can't handle duplicate const/let definitions even between cases.
+
+	if (data.length !== size && type !== DTYPE.NULL && type !== DTYPE.PADDING) throw new RangeError(`Pack expected input of length ${size}, but received ${data.length} instead!`);
+
+	/* 8-BIT INTEGER */
+
+	if (type === DTYPE.UINT8) {
+		return new Uint8Array( data );
+	}
+		
+	if (type === DTYPE.INT8) {
+		return new Uint8Array(new Int8Array(data).buffer);
+	}
+
+	/* 16-BIT INTEGER */
+
+	if (type === DTYPE.UINT16) {
+		if (endianness === system_endianness) return new Uint8Array(new Uint16Array(data).buffer);
+
+		let arr_out = new Uint8Array(data.length*2);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<data.length; i++ ) dataview.setUint16(i*2, data[i], endianness);
+		return arr_out;
+	}
+
+	if (type === DTYPE.INT16) {
+		if (endianness === system_endianness) return new Uint8Array(new Int16Array(data).buffer);
+
+		let arr_out = new Uint8Array(data.length*2);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<data.length; i++ ) dataview.setInt16(i*2, data[i], endianness);
+		return arr_out;
+	}
+
+	/* 32-BIT INTEGER */
+
+	if (type === DTYPE.UINT32) {
+		if (endianness === system_endianness) return new Uint8Array(new Uint32Array(data).buffer);
+
+		let arr_out = new Uint8Array(data.length*4);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<data.length; i++ ) dataview.setUint32(i*4, data[i], endianness);
+		return arr_out;
+	}
+
+	if (type === DTYPE.INT32) {
+		if (endianness === system_endianness) return new Uint8Array(new Int32Array(data).buffer);
+
+		let arr_out = new Uint8Array(data.length*4);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<data.length; i++ ) dataview.setInt32(i*4, data[i], endianness);
+		return arr_out;
+	}
+
+	/* FLOATS */
+
+	if (type === DTYPE.FLOAT32) {
+		if (endianness === system_endianness) return new Uint8Array(new Float32Array(data).buffer);
+
+		let arr_out = new Uint8Array(data.length*4);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<data.length; i++ ) dataview.setFloat32(i*4, data[i], endianness);
+		return arr_out;
+	}
+
+	/* STRINGS */
+
+	if (type === DTYPE.CHAR) {
+		return new Uint8Array(data);
+	}
+
+	if (type === DTYPE.STR) {
+		return TEncoder.encode(data);
+	}
+
+	/* PADDING */
+
+	if (type === DTYPE.NULL) {
+		return new Uint8Array(0);
+	}
+
+	if (type === DTYPE.PADDING) {
+		return new Uint8Array(size).fill(0x00);
+	}
+
+	/* NOT FOUND */
+
+	throw('Unrecognized datatype '+type+'!');
+}
+
+/** Datatype pack function, used internally by Construct. */
+export function Unpack( type:int, data:Uint8Array, size:number, endianness:boolean ): ArrayLike<number>|any|null {
+
+	if (data.length !== size*DSIZE[type] && type !== DTYPE.NULL && type !== DTYPE.PADDING) throw new RangeError(`Unpack expected input of length ${size}, but received ${data.length} instead!`);
+
+	/* 8-BIT INTEGER */
+
+	if (type === DTYPE.UINT8) {
+		return data;
+	}
+		
+	if (type === DTYPE.INT8) {
+		return new Int8Array(data.buffer);
+	}
+
+	/* 16-BIT INTEGER */
+
+	if (type === DTYPE.UINT16) {
+		if (endianness === system_endianness) return new Uint16Array(data.buffer);
+
+		let arr_out = new Uint16Array(size);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<size; i++ ) dataview.getUint16(i*2, endianness);
+		return arr_out;
+	}
+
+	if (type === DTYPE.INT16) {
+		if (endianness === system_endianness) return Array.from(new Int16Array(data.buffer));
+
+		let arr_out = new Int16Array(size);
+		let dataview = new DataView(arr_out.buffer);
+		for ( let i=0; i<size; i++ ) dataview.getInt16(i*2, endianness);
+		return arr_out;
+	}
+
+	/* PADDING */
+
+	if (type === DTYPE.NULL) {
+		return new Array(size).fill(null);
+	}
+
+	if (type === DTYPE.PADDING) {
+		return null;
+	}
+	
+	throw('Unrecognized datatype '+type+'!');
+}
